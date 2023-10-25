13b3be9db0e5e9cb7efdbd39deb085ec4cf7a5a7
ShadowNearby <yan.jingsheng@outlook.com>
2023-10-25 21:17:13 +0800

    format

--- src/filesystem/control_op.cc
+++ src/filesystem/control_op.cc
@@ -3,44 +3,34 @@
 
 namespace chfs {
 
-FileOperation::FileOperation(std::shared_ptr<BlockManager> bm,
-                             u64 max_inode_supported)
-    : block_manager_(bm), inode_manager_(std::shared_ptr<InodeManager>(
-                              new InodeManager(bm, max_inode_supported))),
-      block_allocator_(std::shared_ptr<BlockAllocator>(
-          new BlockAllocator(bm, inode_manager_->get_reserved_blocks()))) {
+FileOperation::FileOperation(std::shared_ptr<BlockManager> bm, u64 max_inode_supported)
+    : block_manager_(bm),
+      inode_manager_(std::shared_ptr<InodeManager>(new InodeManager(bm, max_inode_supported))),
+      block_allocator_(std::shared_ptr<BlockAllocator>(new BlockAllocator(bm, inode_manager_->get_reserved_blocks()))) {
   // now initialize the superblock
   SuperBlock(bm, inode_manager_->get_max_inode_supported()).flush(0).unwrap();
 }
 
-auto FileOperation::create_from_raw(std::shared_ptr<BlockManager> bm)
-    -> ChfsResult<std::shared_ptr<FileOperation>> {
+auto FileOperation::create_from_raw(std::shared_ptr<BlockManager> bm) -> ChfsResult<std::shared_ptr<FileOperation>> {
   // 1. get the metadata from the super block
   auto superblock_res = SuperBlock::create_from_existing(bm, 0);
   if (superblock_res.is_err()) {
-    return ChfsResult<std::shared_ptr<FileOperation>>(
-        superblock_res.unwrap_error());
+    return ChfsResult<std::shared_ptr<FileOperation>>(superblock_res.unwrap_error());
   }
 
   // 2. create the innode manager
-  auto inode_manager_res = InodeManager::create_from_block_manager(
-      bm, superblock_res.unwrap()->get_ninodes());
+  auto inode_manager_res = InodeManager::create_from_block_manager(bm, superblock_res.unwrap()->get_ninodes());
   if (inode_manager_res.is_err()) {
-    return ChfsResult<std::shared_ptr<FileOperation>>(
-        inode_manager_res.unwrap_error());
+    return ChfsResult<std::shared_ptr<FileOperation>>(inode_manager_res.unwrap_error());
   }
 
   auto reserved_block_num = inode_manager_res.unwrap().get_reserved_blocks();
-  return ChfsResult<std::shared_ptr<FileOperation>>(
-      std::shared_ptr<FileOperation>(new FileOperation(
-          bm, InodeManager::to_shared_ptr(inode_manager_res.unwrap()),
-          std::shared_ptr<BlockAllocator>(
-              new BlockAllocator(bm, reserved_block_num, false)))));
+  return ChfsResult<std::shared_ptr<FileOperation>>(std::shared_ptr<FileOperation>(
+      new FileOperation(bm, InodeManager::to_shared_ptr(inode_manager_res.unwrap()),
+                        std::shared_ptr<BlockAllocator>(new BlockAllocator(bm, reserved_block_num, false)))));
 }
 
-auto FileOperation::get_free_inode_num() const -> ChfsResult<u64> {
-  return inode_manager_->free_inode_cnt();
-}
+auto FileOperation::get_free_inode_num() const -> ChfsResult<u64> { return inode_manager_->free_inode_cnt(); }
 
 auto FileOperation::get_free_blocks_num() const -> ChfsResult<u64> {
   return ChfsResult<u64>(block_allocator_->free_block_cnt());
@@ -72,17 +62,15 @@ auto FileOperation::remove_file(inode_id_t id) -> ChfsNullResult {
   if (inode_p->blocks[inode_p->get_direct_block_num()] != KInvalidBlockID) {
     // we still need to release the indirect block
     std::vector<u8> indirect_block;
-    auto read_res = this->block_manager_->read_block(
-        inode_p->blocks[inode_p->get_direct_block_num()],
-        indirect_block.data());
+    auto read_res =
+        this->block_manager_->read_block(inode_p->blocks[inode_p->get_direct_block_num()], indirect_block.data());
     if (read_res.is_err()) {
       error_code = read_res.unwrap_error();
       goto err_ret;
     }
 
     auto block_p = reinterpret_cast<block_id_t *>(indirect_block.data());
-    for (uint i = 0;
-         i < this->block_manager_->block_size() / sizeof(block_id_t); ++i) {
+    for (uint i = 0; i < this->block_manager_->block_size() / sizeof(block_id_t); ++i) {
       if (block_p[i] == KInvalidBlockID) {
         break;
       } else {
@@ -113,4 +101,4 @@ err_ret:
   return ChfsNullResult(error_code);
 }
 
-} // namespace chfs
+}  // namespace chfs
